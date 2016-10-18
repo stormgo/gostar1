b9bf81ba731d300157af2fafd14ab0aaaf228c64
Michael Angerman <stormasm@gmail.com>
2016-10-18 14:05:54 -0400

    getting all of the links, now just grab the ones in the area you want, this will require using xpath

--- fetch.go
+++ fetch.go
@@ -2,18 +2,42 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	//"io/ioutil"
 	"net/http"
+    "golang.org/x/net/html"
 )
 
 func main() {
 
-	url := "https://godoc.org/github.com/garyburd/redigo/redis?importers"
+	//url := "https://godoc.org/github.com/garyburd/redigo/redis?importers"
+	url := "https://godoc.org/github.com/attic-labs/testify/suite?importers"
 
 	resp, _ := http.Get(url)
-	bytes, _ := ioutil.ReadAll(resp.Body)
+	//bytes, _ := ioutil.ReadAll(resp.Body)
 
-	fmt.Println("HTML:\n\n", string(bytes))
+	//fmt.Println("HTML:\n\n", string(bytes))
 
-	resp.Body.Close()
+	//resp.Body.Close()
+
+    z := html.NewTokenizer(resp.Body)
+
+    for {
+        tt := z.Next()
+        //fmt.Println(tt)
+
+        switch {
+        case tt == html.ErrorToken:
+        	fmt.Println("End of the document, we're done")
+            return
+        case tt == html.StartTagToken:
+            t := z.Token()
+
+            isAnchor := t.Data == "a"
+            if isAnchor {
+                fmt.Println(t)
+            }
+        }
+
+    }
+
 }
