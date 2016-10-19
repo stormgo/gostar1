f0f4ec46545a147d5be41a399eb356f6b63f6ffc
Michael Angerman <stormasm@gmail.com>
2016-10-19 15:21:29 -0400

    it is working !

--- goq.go
+++ goq.go
@@ -14,7 +14,7 @@ func getSelection(doc *goquery.Document, selector string) (s *goquery.Selection)
 
 func goq1() {
 
-	// url := "https://godoc.org/github.com/garyburd/redigo/redis?importers"
+	//url := "https://godoc.org/github.com/garyburd/redigo/redis?importers"
 	url := "https://godoc.org/github.com/attic-labs/testify/suite?importers"
 
 	doc, err := goquery.NewDocument(url)
@@ -23,7 +23,31 @@ func goq1() {
 	}
 
 	selection := getSelection(doc, "table.table.table-condensed")
-	fmt.Println(selection)
+
+    //mystr, _ := selection.Html()
+    //fmt.Println(mystr)
+
+    node := selection.Get(0)
+    if err != nil {
+		log.Fatal(err)
+	}
+
+    doc = goquery.NewDocumentFromNode(node)
+    selection = getSelection(doc,"a")
+
+    //mystr, _ = selection.Html()
+	//fmt.Println(mystr)
+
+    github := ""
+    selection.Each(func(i int, s *goquery.Selection) {
+      // For each item found, get the band and title
+      //title := s.Find("i").Text()
+      //fmt.Printf("Review %d: %s - %s\n", i, band, title)
+      github, _ = s.Attr("href")
+      fmt.Println(i,github)
+    })
+
+
 }
 
 func main() {
