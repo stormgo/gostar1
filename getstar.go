7c48d43ac8d8c5028da602cdb9f538d1bc703d31
Michael Angerman <stormasm@gmail.com>
2016-10-23 09:50:19 -0400

    up and running with github api rest request

---
+++ getstar.go
@@ -0,0 +1,178 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+	//"net/url"
+)
+
+type Github struct {
+	ID int `json:"id"`
+	Name string `json:"name"`
+	FullName string `json:"full_name"`
+	Owner struct {
+		Login string `json:"login"`
+		ID int `json:"id"`
+		AvatarURL string `json:"avatar_url"`
+		GravatarID string `json:"gravatar_id"`
+		URL string `json:"url"`
+		HTMLURL string `json:"html_url"`
+		FollowersURL string `json:"followers_url"`
+		FollowingURL string `json:"following_url"`
+		GistsURL string `json:"gists_url"`
+		StarredURL string `json:"starred_url"`
+		SubscriptionsURL string `json:"subscriptions_url"`
+		OrganizationsURL string `json:"organizations_url"`
+		ReposURL string `json:"repos_url"`
+		EventsURL string `json:"events_url"`
+		ReceivedEventsURL string `json:"received_events_url"`
+		Type string `json:"type"`
+		SiteAdmin bool `json:"site_admin"`
+	} `json:"owner"`
+	Private bool `json:"private"`
+	HTMLURL string `json:"html_url"`
+	Description string `json:"description"`
+	Fork bool `json:"fork"`
+	URL string `json:"url"`
+	ForksURL string `json:"forks_url"`
+	KeysURL string `json:"keys_url"`
+	CollaboratorsURL string `json:"collaborators_url"`
+	TeamsURL string `json:"teams_url"`
+	HooksURL string `json:"hooks_url"`
+	IssueEventsURL string `json:"issue_events_url"`
+	EventsURL string `json:"events_url"`
+	AssigneesURL string `json:"assignees_url"`
+	BranchesURL string `json:"branches_url"`
+	TagsURL string `json:"tags_url"`
+	BlobsURL string `json:"blobs_url"`
+	GitTagsURL string `json:"git_tags_url"`
+	GitRefsURL string `json:"git_refs_url"`
+	TreesURL string `json:"trees_url"`
+	StatusesURL string `json:"statuses_url"`
+	LanguagesURL string `json:"languages_url"`
+	StargazersURL string `json:"stargazers_url"`
+	ContributorsURL string `json:"contributors_url"`
+	SubscribersURL string `json:"subscribers_url"`
+	SubscriptionURL string `json:"subscription_url"`
+	CommitsURL string `json:"commits_url"`
+	GitCommitsURL string `json:"git_commits_url"`
+	CommentsURL string `json:"comments_url"`
+	IssueCommentURL string `json:"issue_comment_url"`
+	ContentsURL string `json:"contents_url"`
+	CompareURL string `json:"compare_url"`
+	MergesURL string `json:"merges_url"`
+	ArchiveURL string `json:"archive_url"`
+	DownloadsURL string `json:"downloads_url"`
+	IssuesURL string `json:"issues_url"`
+	PullsURL string `json:"pulls_url"`
+	MilestonesURL string `json:"milestones_url"`
+	NotificationsURL string `json:"notifications_url"`
+	LabelsURL string `json:"labels_url"`
+	ReleasesURL string `json:"releases_url"`
+	DeploymentsURL string `json:"deployments_url"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	PushedAt time.Time `json:"pushed_at"`
+	GitURL string `json:"git_url"`
+	SSHURL string `json:"ssh_url"`
+	CloneURL string `json:"clone_url"`
+	SvnURL string `json:"svn_url"`
+	Homepage string `json:"homepage"`
+	Size int `json:"size"`
+	StargazersCount int `json:"stargazers_count"`
+	WatchersCount int `json:"watchers_count"`
+	Language string `json:"language"`
+	HasIssues bool `json:"has_issues"`
+	HasDownloads bool `json:"has_downloads"`
+	HasWiki bool `json:"has_wiki"`
+	HasPages bool `json:"has_pages"`
+	ForksCount int `json:"forks_count"`
+	MirrorURL interface{} `json:"mirror_url"`
+	OpenIssuesCount int `json:"open_issues_count"`
+	Forks int `json:"forks"`
+	OpenIssues int `json:"open_issues"`
+	Watchers int `json:"watchers"`
+	DefaultBranch string `json:"default_branch"`
+	Organization struct {
+		Login string `json:"login"`
+		ID int `json:"id"`
+		AvatarURL string `json:"avatar_url"`
+		GravatarID string `json:"gravatar_id"`
+		URL string `json:"url"`
+		HTMLURL string `json:"html_url"`
+		FollowersURL string `json:"followers_url"`
+		FollowingURL string `json:"following_url"`
+		GistsURL string `json:"gists_url"`
+		StarredURL string `json:"starred_url"`
+		SubscriptionsURL string `json:"subscriptions_url"`
+		OrganizationsURL string `json:"organizations_url"`
+		ReposURL string `json:"repos_url"`
+		EventsURL string `json:"events_url"`
+		ReceivedEventsURL string `json:"received_events_url"`
+		Type string `json:"type"`
+		SiteAdmin bool `json:"site_admin"`
+	} `json:"organization"`
+	NetworkCount int `json:"network_count"`
+	SubscribersCount int `json:"subscribers_count"`
+}
+
+func main() {
+
+	//phone := "14158586273"
+
+	// QueryEscape escapes the phone string so
+	// it can be safely placed inside a URL query
+	//safePhone := url.QueryEscape(phone)
+	//url := fmt.Sprintf("http://apilayer.net/api/validate?access_key=YOUR_ACCESS_KEY&number=%s", safePhone)
+
+	url := fmt.Sprintf("https://api.github.com/repos/attic-labs/noms")
+
+
+	// Build the request
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal("NewRequest: ", err)
+		return
+	}
+
+	// For control over HTTP client headers,
+	// redirect policy, and other settings,
+	// create a Client
+	// A Client is an HTTP client
+	client := &http.Client{}
+
+	// Send the request via a client
+	// Do sends an HTTP request and
+	// returns an HTTP response
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Do: ", err)
+		return
+	}
+
+	// Callers should close resp.Body
+	// when done reading from it
+	// Defer the closing of the body
+	defer resp.Body.Close()
+
+	// Fill the record with the data from the JSON
+	var record Github
+
+	// Use json.Decode for reading streams of JSON data
+	if err := json.NewDecoder(resp.Body).Decode(&record); err != nil {
+		log.Println(err)
+	}
+
+	fmt.Println("Stars = ", record.StargazersCount)
+
+/*
+	fmt.Println("Phone No. = ", record.InternationalFormat)
+	fmt.Println("Country   = ", record.CountryName)
+	fmt.Println("Location  = ", record.Location)
+	fmt.Println("Carrier   = ", record.Carrier)
+	fmt.Println("LineType  = ", record.LineType)
+*/
+}
